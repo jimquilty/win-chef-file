e09c3fb49c172c1bda6320318f07587d2e2acb53
Jim Quilty <jquilty@chef.io>
2020-07-22 13:30:30 -0400

    Initial commit
    Signed-off-by: Jim Quilty <jquilty@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'win-chef-file'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Jim Quilty'
+maintainer_email 'jquilty@chef.io'
 license 'All Rights Reserved'
-description 'Installs/Configures win-chef-file'
+description 'Creates the chef.txt file'
 version '0.1.0'
 chef_version '>= 15.0'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+file 'C:\chef.txt' do
+  content 'Chef is Great!'
+  rights :read, 'Everyone'
+  rights :write, 'Everyone'
+  action :create
+end
